c14932ff364f0ef359816d9c065407cca32914ae
Alex Lin <alex.lin9991@gmail.com>
2023-02-04 17:23:00 +1100

    set picture height and width

--- src/components/hero/gallery.jsx
+++ src/components/hero/gallery.jsx
@@ -3,15 +3,24 @@ const Gallery = () => {
     <div className="-mb-96 hidden flex-row gap-4 lg:flex">
       <div>
         <img
+          height="742px"
+          width="668px"
           src={"/images/gallery1.webp"}
           alt="Example website - physiotherapy clinic"
         />
       </div>
       <div>
-        <img src={"/images/gallery2.webp"} alt="Example website - cafe" />
+        <img
+          height="742px"
+          width="668px"
+          src={"/images/gallery2.webp"}
+          alt="Example website - cafe"
+        />
       </div>
       <div>
         <img
+          height="742px"
+          width="668px"
           src={"/images/gallery3.webp"}
           alt="Example website - coffee shop"
         />
