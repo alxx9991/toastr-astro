ebe31c1fbb729c22eda4620a5a6425b94560d0d4
Alex Lin <alex.lin9991@gmail.com>
2023-02-04 17:45:35 +1100

    include astro in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/layouts/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx,astro}",
+    "./src/components/**/*.{js,jsx,ts,tsx,astro}",
+    "./src/layouts/**/*.{js,jsx,ts,tsx,astro}",
   ],
   theme: {
     fontFamily: {
