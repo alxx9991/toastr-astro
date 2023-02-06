90e159e9ba43c1535c4a3a3eae072ccf8414a8be
Alex Lin <alex.lin9991@gmail.com>
2023-02-06 19:23:14 +1100

    fix hero decoration transform bug

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -11,22 +11,32 @@ const Hero = () => {
     <>
       <div className="full-width-section bg-bg pt-16 text-center md:pt-32 lg:mb-48 lg:pt-24">
         <Row className="relative mb-16 flex flex-col gap-16 md:mb-12 md:gap-20 md:px-16 lg:gap-8">
-          <div className="absolute inset-0 hidden -translate-x-20 translate-y-20 animate-fadeFromLeft md:block">
-            <img src={leftThingSvgReference} alt="left decoration" />
+          <div className="absolute inset-0 hidden -translate-x-20 translate-y-20 md:block">
+            <img
+              src={leftThingSvgReference}
+              alt="left decoration"
+              className="animate-fadeFromLeft"
+            />
           </div>
-          <div className="absolute left-0 bottom-0 block -translate-y-16 animate-fadeFromLeft md:hidden">
+          <div className="absolute left-0 bottom-0 block -translate-y-16 md:hidden">
             <img
               src={leftThingMobileSvgReference}
               alt="left decoration mobile"
+              className="animate-fadeFromLeft"
             />
           </div>
-          <div className="absolute right-0 top-0 hidden translate-x-20 -translate-y-40 animate-fadeFromRight overflow-x-visible md:block lg:-translate-y-24">
-            <img src={rightThingSvgReference} alt="right decoration" />
+          <div className="absolute right-0 top-0 hidden translate-x-20 -translate-y-40 overflow-x-visible md:block lg:-translate-y-24">
+            <img
+              src={rightThingSvgReference}
+              alt="right decoration"
+              className="animate-fadeFromRight"
+            />
           </div>
-          <div className="absolute right-0 top-0 block translate-x-1 -translate-y-14 animate-fadeFromRight overflow-x-visible md:hidden">
+          <div className="absolute right-0 top-0 block translate-x-1 -translate-y-14 overflow-x-visible md:hidden">
             <img
               src={rightThingMobileSvgReference}
               alt="right decoration mobile"
+              className="animate-fadeFromRight"
             ></img>
           </div>
           <div className="relative z-10 flex animate-fadeFromBottom flex-col px-6 md:gap-2 md:px-0 lg:gap-2">
